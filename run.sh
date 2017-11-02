8e3c810319a7572e268b9ab6ddd28f298e52af1c
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-11-01 18:15:32 -0700

    Remove all unused data files

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 SEMPREDIR=`dirname $0`
 SEMPREDIR=`realpath $SEMPREDIR`
-MODULE=${MODULE:-almond}
 EXTRA_ARGS="$@"
 JAVA=${JAVA:-java}
 
-exec ${JAVA} -ea ${JAVA_ARGS} -Djava.library.path=${SEMPREDIR}/jni -cp ${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/* edu.stanford.nlp.sempre.TokenizerServer ++${MODULE}/${MODULE}.tokenizer.conf -SpellCheckerAnnotator.dictionaryDirectory ${SEMPREDIR}/myspell ${EXTRA_ARGS}
+exec ${JAVA} -ea ${JAVA_ARGS} -Djava.library.path=${SEMPREDIR}/jni -cp ${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/* edu.stanford.nlp.sempre.TokenizerServer ++data/default.conf -SpellCheckerAnnotator.dictionaryDirectory ${SEMPREDIR}/myspell ${EXTRA_ARGS}
