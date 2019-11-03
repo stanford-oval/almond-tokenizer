4d9484ada26670b77eb31ddaef812cdb555931f4
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-11-03 11:49:35 -0800

    run: enable Italian by default

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ SEMPREDIR=`dirname $0`
 SEMPREDIR=`realpath $SEMPREDIR`
 EXTRA_ARGS="$@"
 JAVA=${JAVA:-java}
-LANGUAGES=${LANGUAGES:-en zh-hans zh-hant}
+LANGUAGES=${LANGUAGES:-en zh-hans zh-hant it}
 PORT=${PORT:-8888}
 
 exec ${JAVA} -Xmx7G -ea ${JAVA_ARGS} -cp ${SEMPREDIR}/libsempre/*:${SEMPREDIR}/lib/* edu.stanford.nlp.sempre.TokenizerServer --port ${PORT} ${LANGUAGES}
